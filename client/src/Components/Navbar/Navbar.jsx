aa6179d53bdfeec73e99c747421e67f87bce8693
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2024-01-16 00:51:33 +0530

    add tailwind to components

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -5,24 +5,25 @@ import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="container1">
-        <div className="home">
-          <div className="tracker-info">
+    <nav className="flex justify-between items-center p-[10px]">
+      <div className="flex items-center">
+        <div className="mr-[10px]">
+          <div className="flex items-center px-3">
             <FontAwesomeIcon
               icon={faLock}
               style={{ color: "#313539" }}
               size="2x"
             />
-            <span className="tracker-text">Tracker</span>
+            <span className="ml-2 mt-[5px] font-bold text-[20px]">Tracker</span>
           </div>
         </div>
       </div>
       <div className="navbar-right">
-        <button type="button" className="button">
-          <div className="button-top">Get Started</div>
-          <div className="button-bottom"></div>
-          <div className="button-base"></div>
+        <button
+          type="button"
+          className="px-3 py-2 rounded-lg mx-3 hover:bg-blue-200 transition-all duration-300 ease-in-out transform hover:scale-95 font-semibold text-[19px]"
+        >
+          Get Started
         </button>
       </div>
     </nav>
